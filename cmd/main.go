006af4692372877e0190ce3dcfa41230d0634557
Jürgen Ryannel <juergen@ryannel.org>
2022-09-07 09:55:12 +0200

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -13,14 +13,14 @@ func main() {
 	done := make(chan struct{})
 	emitter := events.New[EventA]()
 	// register a new event handler
-	unsubscribe := emitter.On(func(data EventA) {
+	off := emitter.On(func(data EventA) {
 		fmt.Println(data.Message)
 		// exit after first event
 		close(done)
 	})
 	// emit an event
 	emitter.Emit(EventA{"hello"})
-	unsubscribe() // unsubscribe handler
+	off() // unsubscribe handler
 	// emit another event, but no handler is registered now
 	emitter.Emit(EventA{"hello"})
 	<-done
